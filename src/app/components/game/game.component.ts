8bc383b11342b7e24a41abc5f4b288aaa5ee0f4c
Ontiveros Alfaro Johan Alejandro <johalfaro1174@gmail.com>
2025-08-06 08:22:51 -0600

    Style fix.

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -113,7 +113,7 @@ template: `
 
             <button class="download-btn cta-button" style="width: 100%;">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="24" role="presentation" alt="" data-testid="DownloadIcon" color="currentColor" size="24"><path d="M4.3 19.4v5.3c0 .5.5.9 1 .9h21.4c.5 0 1-.5 1-.9v-5.3H32v6.3c-.2 2.2-2.1 4-4.4 4H4.2c-2.3 0-4.2-1.8-4.2-4v-6.3h4.3zM17.9 2.2v12.2l5-4.8 2.8 2.8-9.8 9.9L6 12.5l2.6-2.8 5.2 4.8V2.2h4.1z" fill-rule="evenodd" clip-rule="evenodd" fill="currentColor"></path></svg>
-              Direct download
+              <span class="fs-5 ms-2"> Direct download</span>
             </button>
             
             <p class="fs-6 fw-lighter">This item will be sent to your system automatically after purchase.</p>
